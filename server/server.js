924b30a276c55cfd61ba830185a6ed2e38584650
Manuel Sand <manuelcarlossandoval@gmail.com>
2020-07-12 17:11:43 -0300

    Send broadcast from Server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const socketIO = require("socket.io");
+//socket doesn't work with express, but it works with http server integrated in node
+const http = require("http");
+
+const path = require("path");
+
+const app = express();
+//since express, behind, uses http commands, and since the instructions are so similar,
+//it can be passed to the http for create the server
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, "../public");
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+//initialize the socket. Is the backend communication
+module.exports.io = socketIO(server);
+require("./sockets/socket");
+
+server.listen(port, (err) => {
+  if (err) throw new Error(err);
+
+  console.log(`Servidor corriendo en puerto ${port}`);
+});
---
+++ server/sockets/socket.js
@@ -0,0 +1,22 @@
+const { io } = require("../server");
+//to know when a client is connected to the server
+io.on("connection", (client) => {
+  console.log("User connected");
+
+  client.emit("sendMessage", {
+    user: "Admin",
+    message: "Welcome!",
+  });
+
+  client.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  client.on("sendMessage", (data, cb) => {
+    /* data.name
+      ? cb({ name: "server", message: "Everithing is OK" })
+      : cb({ name: "server", message: "Something is wrong" }); */
+      console.log(data);
+    client.broadcast.emit("sendMessage", data);
+  });
+});
